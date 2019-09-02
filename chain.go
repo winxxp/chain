4e7ca5eb6746811f7df8ae589be02558dc06b30d
winxxp <haolei2008@gmail.com>
2019-09-02 15:38:58 +0800

    chain独立成包

--- chain.go
+++ chain.go
@@ -2,27 +2,41 @@ package chain
 
 type Handle func() error
 
-type C struct {
-	err     error
-	handles []Handle
+func Run(h ...Handle) error {
+	return New().Handles(h...).Run()
 }
 
-func New() *C {
-	return &C{
+type HandleChain struct {
+	err        error
+	handles    []Handle
+	lastHandle func(error)
+}
+
+func New() *HandleChain {
+	return &HandleChain{
 		handles: make([]Handle, 0, 3),
 	}
 }
 
-func (r *C) Handles(h ...Handle) *C {
+func (r *HandleChain) Last(h func(error)) *HandleChain {
+	r.lastHandle = h
+	return r
+}
+
+func (r *HandleChain) Handles(h ...Handle) *HandleChain {
 	r.handles = append(r.handles, h...)
 	return r
 }
 
-func (c *C) Run() error {
+func (c *HandleChain) Run() error {
 	for _, h := range c.handles {
 		if c.err == nil {
 			c.err = h()
 		}
+	}
+
+	if c.lastHandle != nil {
+		c.lastHandle(c.err)
 	}
 
 	return c.err
--- chain_test.go
+++ chain_test.go
@@ -2,33 +2,56 @@ package chain
 
 import (
 	"errors"
-	"github.com/smartystreets/goconvey/convey"
+	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
 
 func TestC_Handles(t *testing.T) {
-	convey.Convey("Chain", t, func() {
+	Convey("Chain", t, func() {
 		var (
 			val     = 0
 			errTest = errors.New("test")
 		)
+
+		last := func(err error) {
+			val = 0
+			t.Log("Last error: ", err)
+		}
 
 		f1 := func() error { val++; return nil }
 		f2 := func(i int) { val += i }
 		f3 := func() error { return errTest }
 		f4 := func() error { val++; return nil }
 
-		convey.Convey("Error", func() {
+		Convey("Error", func() {
 			err := New().Handles(f1, func() error { f2(2); return nil }, f3, f4).Run()
-			convey.So(err, convey.ShouldBeError, errTest)
-			convey.So(val, convey.ShouldEqual, 3)
+			So(err, ShouldBeError, errTest)
+			So(val, ShouldEqual, 3)
 		})
 
-		convey.Convey("No Error", func() {
+		Convey("No Error", func() {
 			err := New().Handles(f1, f4).Run()
-			convey.So(err, convey.ShouldBeNil)
-			convey.So(val, convey.ShouldEqual, 2)
+			So(err, ShouldBeNil)
+			So(val, ShouldEqual, 2)
 		})
+
+		Convey("Add Last", func() {
+			r := New().Last(last)
+			err := r.Handles(
+				f1, func() error {
+					f2(10)
+					return nil
+				},
+				f3).Run()
+			So(val, ShouldEqual, 0)
+			So(err, ShouldEqual, errTest)
+		})
+
+		Convey("Run", func() {
+			err := Run(f1, f4)
+			So(err, ShouldBeNil)
+			So(val, ShouldEqual, 2)
+		})
+
 	})
 }
-
